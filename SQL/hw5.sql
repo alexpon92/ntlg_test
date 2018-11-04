c9cdeca3eabe158401104ca355d78009b88edf00
alexpon92 <alexpon92@gmail.com>
2018-11-04 23:15:58 +0300

    Update hw5.sql

--- SQL/hw5.sql
+++ SQL/hw5.sql
@@ -10,3 +10,16 @@ WHERE table_type = 'BASE TABLE'
 ORDER BY real_size DESC
 LIMIT 5;
 
+-- Используя функцию array_agg собрать в массив все фильмы, просмотренные пользователем. 
+-- При этом повторов в списке контента быть не должно. Назовём эту конструкцию ЗАПРОС1. 
+-- Выборка должна содержать два поля: userid и user_views
+SELECT r.userid, array_agg(movieid) as user_views
+FROM (SELECT userid, movieid FROM ratings GROUP BY userid, movieid) as r
+GROUP BY r.userid;
+
+-- Создайте таблицу user_movies_agg, в которую сохраните результат запроса
+SELECT userid, user_views INTO public.user_movies_agg
+FROM (SELECT r.userid, array_agg(movieid) as user_views
+      FROM (SELECT userid, movieid FROM ratings GROUP BY userid, movieid) as r
+      GROUP BY r.userid) aggregated_views;
+
