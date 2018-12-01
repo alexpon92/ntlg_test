e53a7181cb617a5987fc533ebc82b52279e29d37
alexpon92 <alexpon92@gmail.com>
2018-12-01 17:42:11 +0300

    Create advanced_tasks.py

---
+++ Python/hw1/advanced_tasks.py
@@ -0,0 +1,30 @@
+"""
+В переменных a и b записаны 2 различных числа.
+Вам необходимо написать код, который меняет значения a и b местами без использования третьей переменной.
+"""
+first_val = 123
+sec_val = 5661
+
+# standard XOR algorithm is used
+first_val = first_val ^ sec_val
+sec_val = sec_val ^ first_val
+first_val = first_val ^ sec_val
+
+print(first_val, sec_val)
+
+"""
+Дано число в двоичной системе счисления: num=10011. Напишите алгоритм перевода этого числа в привычную 
+нам десятичную систему счисления.
+Возможно, вам понадобится цикл прохождения всех целых чисел от 0 до m:
+for n in range(m)
+"""
+num = 10011
+
+# handmade algorithm
+num = str(num)[::-1]
+res, pos = 0, 0
+for n in num:
+    res += int(n) * 2 ** (int(pos))
+    pos += 1
+
+print(res)
